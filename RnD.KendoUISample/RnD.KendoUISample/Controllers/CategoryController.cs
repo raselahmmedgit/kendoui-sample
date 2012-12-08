8b0da1b79a57a046269e46ba639f7274f2f536e2
Rasel Ahmmed Bappi <rasel.bappi@hotmail.com>
2012-12-08 14:51:09 +0600

    first commit

---
+++ RnD.KendoUISample/RnD.KendoUISample/Controllers/CategoryController.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RnD.KendoUISample.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using System.Data.Linq;
+
+namespace RnD.KendoUISample.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private AppDbContext _db = new AppDbContext();
+
+        //
+        // GET: /Category/
+
+        public ViewResult Index()
+        {
+            //return View(GetCategorys());
+            return View();
+        }
+
+        //InCell
+        public ViewResult InCell()
+        {
+            //return View(GetCategorys());
+            return View();
+        }
+
+        //PopUp
+        public ViewResult PopUp()
+        {
+            //return View(GetCategorys());
+            return View();
+        }
+
+        //CustomPopUp
+        public ViewResult Dialog()
+        {
+            //return View(GetCategorys());
+            return View();
+        }
+
+        //CustomPopUp By Kendo UI
+        public ViewResult Window()
+        {
+            //return View(GetCategorys());
+            return View();
+        }
+
+        //CustomPopUp By Kendo UI
+        public ViewResult WindowLink()
+        {
+            //return View(GetCategorys());
+            return View();
+        }
+
+        //CustomPopUp By Kendo UI
+        public ViewResult WindowGlobal()
+        {
+            //return View(GetCategorys());
+            return View();
+        }
+
+        public ActionResult Categorys_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(GetCategorys().ToDataSourceResult(request));
+        }
+
+        private IEnumerable<Category> GetCategorys()
+        {
+            var categories = _db.Categories.ToList();
+
+            return categories;
+        }
+
+        #region CRUD Action For InCell By List
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Categorys_Create([DataSourceRequest] DataSourceRequest request, [Bind(Prefix = "models")]IEnumerable<Category> categorys)
+        {
+            var results = new List<Category>();
+
+            if (categorys != null && ModelState.IsValid)
+            {
+                foreach (var category in categorys)
+                {
+                    _db.Categories.Add(category);
+                    _db.SaveChanges();
+                    results.Add(category);
+                }
+            }
+
+            return Json(results.ToDataSourceResult(request, ModelState));
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Categorys_Update([DataSourceRequest] DataSourceRequest request, [Bind(Prefix = "models")]IEnumerable<Category> categorys)
+        {
+            if (categorys != null && ModelState.IsValid)
+            {
+                foreach (var category in categorys)
+                {
+                    var target = _db.Categories.SingleOrDefault(c => c.CategoryId == category.CategoryId);
+                    if (target != null)
+                    {
+                        target.Name = category.Name;
+
+                        _db.Entry(target).State = EntityState.Modified;
+                        _db.SaveChanges();
+                    }
+                }
+            }
+
+            return Json(ModelState.ToDataSourceResult());
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Categorys_Destroy([DataSourceRequest] DataSourceRequest request, [Bind(Prefix = "models")]IEnumerable<Category> categorys)
+        {
+            if (categorys != null && categorys.Any())
+            {
+                foreach (var category in categorys)
+                {
+                    _db.Categories.Remove(category);
+                    _db.SaveChanges();
+                }
+            }
+
+            return Json(ModelState.ToDataSourceResult());
+        }
+
+
+        #endregion
+
+        #region CRUD Action For PopUp
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("PopUp");
+            }
+
+            return View("PopUp");
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Update(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(category).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("PopUp");
+            }
+
+            return View("PopUp");
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Destroy(int categoryID)
+        {
+            Category category = _db.Categories.Find(categoryID);
+
+            if (category != null)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("PopUp");
+        }
+
+        #endregion
+
+        #region CRUD Action For Custom PopUp
+
+        //
+        // GET: /Category/Details/By ID
+
+        public ActionResult Details(int id)
+        {
+            Category category = _db.Categories.Find(id);
+            //return View(category);
+            return PartialView("_Details", category);
+        }
+
+        //
+        // GET: /Category/Create
+
+        public ActionResult Add()
+        {
+            //return View();
+            return PartialView("_Add");
+        }
+
+        //
+        // POST: /Category/Create
+
+        [HttpPost]
+        public ActionResult Add(Category category)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Categories.Add(category);
+                    _db.SaveChanges();
+
+                    //return RedirectToAction("Index");
+                    return Content(Boolean.TrueString);
+                }
+
+                //return View(category);
+                //return PartialView("_Add", category);
+                return Content("Please review your form.");
+            }
+            catch (Exception ex)
+            {
+                return Content("Error Occured!");
+            }
+        }
+
+        //
+        // GET: /Category/Edit/By ID
+
+        public ActionResult Edit(int id)
+        {
+            Category category = _db.Categories.Find(id);
+
+            //return View(category);
+            return PartialView("_Edit", category);
+        }
+
+        //
+        // POST: /Category/Edit/By ID
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _db.Entry(category).State = EntityState.Modified;
+                    _db.SaveChanges();
+
+                    //return RedirectToAction("Index");
+                    return Content(Boolean.TrueString);
+                }
+
+                //return View(category);
+                //return PartialView("_Edit", category);
+                return Content("Please review your form.");
+            }
+            catch (Exception ex)
+            {
+                return Content("Error Occured!");
+            }
+        }
+
+        //
+        // GET: /Category/Delete/By ID
+
+        public ActionResult Delete(int id)
+        {
+            Category category = _db.Categories.Find(id);
+
+            //return View(category);
+            return PartialView("_Delete", category);
+        }
+
+        //
+        // POST: /Category/Delete/By ID
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                Category category = _db.Categories.Find(id);
+                if (category != null)
+                {
+                    _db.Categories.Remove(category);
+                    _db.SaveChanges();
+
+                    //return RedirectToAction("Index");
+                    return Content(Boolean.TrueString);
+                }
+                return Content("Please review your form.");
+            }
+            catch (Exception ex)
+            {
+                return Content("Error Occured!");
+            }
+        }
+
+        #endregion
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ RnD.KendoUISample/RnD.KendoUISample/Models/AppModels.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RnD.KendoUISample.Models
+{
+    public class BaseModel
+    {
+        [NotMapped]
+        public virtual bool KendoWindow { get; set; }
+    }
+
+    public class Category : BaseModel
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Product : BaseModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { get; set; }
+    }
+}
